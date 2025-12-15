292dc79cba9efff8750970d81333a37a9fdcba07
Danny-1211 <danny21316@gmail.com>
2025-12-15 23:34:12 +0800

    refactor: adjust DOM location

--- js/constant.js
+++ js/constant.js
@@ -2,19 +2,10 @@ const API_PATH = 'danny'; // API 使用者路徑
 const API_HEADER = 'https://livejs-api.hexschool.io'; // 前段網域
 const API_PRODUCTS = `${API_HEADER}/api/livejs/v1/customer/${API_PATH}/products`; // 商品列表 
 
-const productWrapDOM = document.querySelector('.productWrap')
-
-
 const path = {
     products: API_PRODUCTS
 }
 
-const dom = {
-    productWarp: productWrapDOM
-}
-
-
 export {
-    path,
-    dom
+    path
 }
