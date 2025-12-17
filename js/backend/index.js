3dc56b3bb5ab7ee4ef27e85bcb307292e2932233
danny <danny21316@gmail.com>
2025-12-17 13:23:02 +0800

    feat: integrate order list api

--- js/backend/index.js
+++ js/backend/index.js
@@ -1,19 +1,61 @@
-// C3.js
-let chart = c3.generate({
-    bindto: '#chart', // HTML 元素綁定
-    data: {
-        type: "pie",
-        columns: [
-        ['Louvre 雙人床架', 1],
-        ['Antony 雙人床架', 2],
-        ['Anty 雙人床架', 3],
-        ['其他', 4],
-        ],
-        colors:{
-            "Louvre 雙人床架":"#DACBFF",
-            "Antony 雙人床架":"#9D7FEA",
-            "Anty 雙人床架": "#5434A7",
-            "其他": "#301E5F",
-        }
-    },
-});
+import { getOrders } from './service.js';
+
+let ordersList = [];
+
+const orderPageTableDom = document.querySelector('.orderPage-table');
+
+const DOM = {
+    orderPageTable: orderPageTableDom
+}
+
+async function init() {
+    ordersList = await getOrders();
+    renderOrderList(ordersList);
+}
+
+function renderOrderList(ordersList) {
+    let temp = `                
+    <thead>
+        <tr>
+            <th>訂單編號</th>
+            <th>聯絡人</th>
+            <th>聯絡地址</th>
+            <th>電子郵件</th>
+            <th>訂單品項</th>
+            <th>訂單日期</th>
+            <th>訂單狀態</th>
+            <th>操作</th>
+        </tr>
+    </thead>
+    `;
+
+    temp += ordersList.orders.map(order => {
+        return `
+            <tr>
+                <td>${order.id}</td>
+                <td>
+                    <p>${order.user.name}</p>
+                    <p>${order.user.tel}</p>
+                </td>
+                <td>${order.user.address}</td>
+                <td>${order.user.email}</td>
+                <td>
+                    ${order.products.map(product => {
+                        return `<p>${product.title} x ${product.quantity}</p>`
+                    }).join('')}
+                </td>
+                <td>${new Date(order.createdAt * 1000).toLocaleDateString()}</td>
+                <td class="orderStatus">
+                    <button type="button">未處理</button>
+                </td>
+                <td>
+                    <input type="button" class="delSingleOrder-Btn" value="刪除">
+                </td>
+            </tr>
+        `
+    }).join('')
+
+    DOM.orderPageTable.innerHTML = temp;
+}
+
+init();
--- js/backend/service.js
+++ js/backend/service.js
@@ -1,1 +1,21 @@
+// 打後台 api 並回傳該 data
+import { PATH, ADMIN_UID } from './constant.js';
 
+async function getOrders() {
+    const para = {
+        headers: {
+            'Authorization': ADMIN_UID
+        }
+    }
+    try {
+        const res = await axios.get(PATH.ORDERS, para);
+        return res.data;
+    } catch (error) {
+        console.log('取得後台訂單列表發生錯誤', error);
+    }
+}
+
+
+export {
+    getOrders
+}
